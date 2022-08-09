6cb394c2b76f13572c4d29cc45baa386a29f3e19
Miroslav <miroslavrilley@gmail.com>
2022-08-09 17:28:48 +0300

    Update project and push after commit two

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Мой новый проект для пуша в Гитхаб.");
+        System.out.println("Обновление проекта и пуш после 2 коммита");
     }
 }
